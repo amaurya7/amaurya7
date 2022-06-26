08f5435139a11b6cf03def47f9a035164a3e067e
amaurya7 <92113221+amaurya7@users.noreply.github.com>
2022-06-27 01:30:43 +0530

    Add files via upload

---
+++ BrowserFactory.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsSelenium
+{
+    public class BrowserFactory
+    {
+        public static IWebDriver driver;
+        public static void OpenBrowser(string Browser)
+        {
+            switch(Browser)
+            {
+                case "Chromer":
+                    BrowserFactory.ChromeBrowser();
+                break;
+                case "Edge":
+                    BrowserFactory.EdgeBrowser();
+                    break;
+                case "Firefox":
+                    BrowserFactory.FirefoxBrowser();
+                break;
+                default:
+                    BrowserFactory.ChromeBrowser();
+                break;
+            }
+        }
+        private static void ChromeBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();            
+        }
+        private static void EdgeBrowser()
+        {
+            driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        private static void FirefoxBrowser()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+        }
+        public static void CloseBrowser()
+        {
+            driver.Quit();
+        }
+        public static void OpenApplication(string URL)
+        {
+            driver.Navigate().GoToUrl(URL);
+        }
+
+        public static void TakeFullScreenshot( string filename)
+        {
+            var screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+            screenshot.SaveAsFile(filename, ScreenshotImageFormat.Png);
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.Collections;
+
+namespace JenkinsSelenium
+{
+    public class Test : BrowserFactory
+    {
+        string inputfile = @"C:\Users\amaurya\source\repos\JenkinsSelenium\TestData\Data.xlsx";
+        string browser = "Chrome";
+        string screenshotname = @"C:\Users\amaurya\source\repos\JenkinsSelenium\TestData\Screenshot.Png";
+
+        [SetUp]
+        public void start_Browser()
+        {
+            BrowserFactory.OpenBrowser(browser);
+        }
+        [TearDown]
+        public void close_Browser()
+        {
+            BrowserFactory.CloseBrowser();
+        }
+        [Test]
+        //public void Initiate(string url, string username, string password)
+        public void Initiate()
+        {
+            var ExcelReaderFile = new ExcelReaderFile(inputfile);
+            int row = ExcelReaderFile.getRowCount("Login");
+            for (int i = 0; i < row; i++)
+            {
+                string url = ExcelReaderFile.getCellData("Login", "URL", 2);
+                string username = ExcelReaderFile.getCellData("Login", "UserName", 2);
+                string password = ExcelReaderFile.getCellData("Login", "Password", 2);
+                Test.login(url, username, password);
+                BrowserFactory.TakeFullScreenshot(screenshotname);
+            }                 
+        }             
+        public static void login(string url, string username, string password)
+        {
+            BrowserFactory.OpenApplication(url);
+            driver.FindElement(By.Id("txtUsername")).SendKeys(username);
+            driver.FindElement(By.Id("txtPassword")).SendKeys(password);
+            driver.FindElement(By.Id("btnLogin")).Click();
+        }
+    }
+}
